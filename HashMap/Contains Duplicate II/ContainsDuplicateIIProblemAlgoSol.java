dc8254ec8f19b3e719081f5c373dae0045952b66
Farhan Pasha <farhanpasha96@gmail.com>
2020-12-30 19:50:21 +0000

    Initial commit for Contains Duplicate II

---
+++ HashMap/Contains Duplicate II/ContainsDuplicateIIProblemAlgoSol.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {        
+        HashMap<Integer,Integer> hs = new HashMap<>();
+
+        for (int i =0;i<nums.length;i++){
+            if(hs.containsKey(nums[i])){
+                int absoluteDifference = Math.abs(hs.get(nums[i]) - i);
+                if(absoluteDifference <= k){
+                    return true;
+                }
+            }
+            hs.put(nums[i],i); //Update the value by latest index
+        }
+        return false;
+    }
+}
