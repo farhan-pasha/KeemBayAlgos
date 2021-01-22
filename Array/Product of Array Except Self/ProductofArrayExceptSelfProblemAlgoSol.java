b0f80e97c7d2f234b9bdf75ad68a69d59c2daecc
Farhan Pasha <farhanpasha96@gmail.com>
2021-01-22 23:30:31 +0000

    Fixed the problem link

--- Array/Product of Array Except Self/ProductofArrayExceptSelfProblemAlgoSol.java
+++ Array/Product of Array Except Self/ProductofArrayExceptSelfProblemAlgoSol.java
@@ -1,19 +1,30 @@
-//Time Complexity: O(n^2)
-//Space Complexity: O(n^2)
+//Time Complexity: O(n)
+//Space Complexity: O(n)
 
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int retArray[] = new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            int prod = 1;
-            for (int j=0;j<nums.length;j++){
-                 if (j!=i) {
-                     prod *= nums[j];
-                 }
-            }
-            retArray[i]=prod; 
+        int leftToRight[] = new int[nums.length];
+        int rightToLeft[] = new int[nums.length];
+
+        int leftToRightProd =1;
+        leftToRight[0]=1;
+        for(int i=1;i<nums.length;i++){
+            leftToRightProd = leftToRightProd*nums[i-1];
+            leftToRight[i] =  leftToRightProd;
         }
-        return retArray;
+        
+        int rightToLeftProd = 1;
+        rightToLeft[nums.length-1]=1;
+        for(int i=nums.length-2;i>=0;i--){
+            rightToLeftProd = rightToLeftProd*nums[i+1];
+            rightToLeft[i] =  rightToLeftProd;
+        }
+        
+        for(int i=0;i<nums.length;i++){
+            nums[i]=leftToRight[i]*rightToLeft[i];
+        }
+        
+        return nums;
     }
 }
 
