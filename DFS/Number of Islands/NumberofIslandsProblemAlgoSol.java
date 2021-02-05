3b52c4d69864f08df87b2ea5396f14e3186740c0
Farhan Pasha <farhanpasha96@gmail.com>
2021-02-05 00:03:36 +0000

    Java solution using dfs

---
+++ DFS/Number of Islands/NumberofIslandsProblemAlgoSol.java
@@ -0,0 +1,30 @@
+//Time Complexity: O()
+//Space Complexity:O()
+
+class Solution {
+    public int numIslands(char[][] grid) {
+        int finalCount=0;
+        for (int i=0;i<grid.length;i++){
+            for (int j=0;j<grid[0].length;j++){
+                if (grid[i][j]=='1') {
+                    finalCount += dfs(grid,i,j);
+                }
+            }
+        }
+        return finalCount;
+    }
+    
+    int dfs(char[][] grid,int i,int j) {
+        if ( j<0 || i<0 || i>=grid.length || j>=grid[0].length ||grid[i][j] == '0'){
+            return 0;
+        }
+        grid[i][j]='0';
+
+        dfs(grid,i,j-1);
+        dfs(grid,i,j+1);
+        dfs(grid,i-1,j);
+        dfs(grid,i+1,j);
+        
+        return 1;
+    }
+}
