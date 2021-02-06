5bfcf255062a58b985a4fce91953c0078172fd36
Farhan Pasha <farhanpasha96@gmail.com>
2021-02-06 02:38:53 +0000

    Fixed the .txt file + added the code to .java file

--- DFS/Surrounded Regions/SurroundedRegionsProblemAlgoSol.java
+++ DFS/Surrounded Regions/SurroundedRegionsProblemAlgoSol.java
@@ -1,25 +1,75 @@
 //Time Complexity: O()
 //Space Complexity:O()
 
-Given a 2D board containing 'X' and 'O' (the letter O), capture all regions surrounded by 'X'.
+class Solution {
+    public void solve(char[][] board) {
+        if(board == null || board.length==0){
+            return;
+        }
+                
+        for(int i=0;i<board.length;i++){
+            if(board[i][0] == 'O'){ //first row
+                //board[i][0]='n';
+                dfs(board,i,0);
+            }
+            if(board[i][board[0].length-1] == 'O'){ //last row
+                //board[i][board[0].length-1]='n';
+                dfs(board,i,board[0].length-1);
+            }
+        }
+        
+        for(int j = 0; j<board[0].length; j++){ //first column
+            if(board[0][j] == 'O'){
+                //board[0][j]='n';
+                dfs(board,0,j);
+            }
+            if(board[board.length-1][j] == 'O'){ //last column
+                //board[board.length-1][j]='n';
+                System.out.println("j: " +(board.length-1)+ " j: " +j);
+                dfs(board,board.length-1,j);
+            }
+        }
 
-A region is captured by flipping all 'O's into 'X's in that surrounded region.
+        flip(board);
+        
+        return ;
+    }
+    
+    
+    public void dfs(char [][]board,int i,int j){
+        //System.out.println("i: " +i+ " j: " +j);
 
-Example:
+        if(i<0 || j<0 || i>=board.length || j >= board[0].length || board[i][j] == 'X' || board[i][j] == 'n' ){
+            return;
+        }
 
-X X X X
-X O O X
-X X O X
-X O X X
-After running your function, the board should be:
+        board[i][j]='n';//no change
 
-X X X X
-X X X X
-X X X X
-X O X X
-Explanation:
+        dfs(board,i+1,j);
+        dfs(board,i-1,j);
+        dfs(board,i,j+1);
+        dfs(board,i,j-1);
+        
+        return;
+    }
+    
+    public void flip(char[][] board){
+        for (int i =0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                //System.out.println("board: " +board[i][j]);
 
-Surrounded regions shouldn’t be on the border, 
-which means that any 'O' on the border of the board are not flipped to 'X'. 
-Any 'O' that is not on the border and it is not connected to an 'O' on the border will be flipped to 'X'. 
-Two cells are connected if they are adjacent cells connected horizontally or vertically.
+                if(board[i][j] == 'O'){ //This should be flipped first, else it can interfere with next if stmt
+                    board[i][j] ='X';
+                }
+
+                if(board[i][j] == 'n'){
+                    board[i][j]='O';
+                } 
+
+            }
+            
+        }
+        
+    }
+    
+}
